edcfb4b8d8114734b1f29ef07f6d2e55e3e0a78b
tawichi <otai0811@gmail.com>
2020-03-08 05:37:03 -0700

    [AC] hitachi2020 hitachi2020_a

---
+++ atcoder.jp/hitachi2020/hitachi2020_a/Main.py
@@ -0,0 +1,14 @@
+s=input()
+ans="No"
+if len(s)==2 and s=="hi":
+  ans="Yes"
+if len(s)==4 and s=="hihi":
+  ans="Yes"
+if len(s)==6 and s=="hihihi":
+  ans="Yes"
+if len(s)==8 and s=="hihihihi":
+  ans="Yes"
+if len(s)==10 and s=="hihihihihi":
+  ans="Yes"
+print(ans)
+
