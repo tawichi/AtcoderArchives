decc9e2b597266749059fc802f056b00de63a427
tawichi <otai0811@gmail.com>
2020-05-04 03:38:55 -0700

    [AC] abs abc088_b

---
+++ atcoder.jp/abs/abc088_b/Main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+  int N;
+  int a[210];
+  cin >> N;
+  for (int i = 0; i < N; ++i) cin >> a[i];
+  sort( a, a+N, greater<int>());
+  int Alice = 0;
+  int Bob = 0;
+  for (int i = 0; i < N; ++i){
+    
+    if (i % 2 == 0){
+      Alice += a[i];
+    }
+    else Bob += a[i];
+  }
+  cout << Alice - Bob << endl;
+}
+  
+    
