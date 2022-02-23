e91bfce34e08def16fa08598c3923e490ad333d5
tawichi <otai0811@gmail.com>
2022-02-22 23:52:41 -0800

    [AC] abc240 abc240_a

---
+++ atcoder.jp/abc240/abc240_a/Main.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ 
+int main(){
+  int a,b;
+  cin>>a>>b;
+  if(abs(a-b)==1 || abs(a-b)==9) cout<<"Yes"<<endl;
+  else cout<<"No"<<endl;
+  
+  return 0;
+}
